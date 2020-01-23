4fb5aace2e30ca7b7f42b40d3b1f94e133d82806
pierrehanne <43729077+pierrehanne@users.noreply.github.com>
2020-01-23 11:03:04 +0100

    V2

--- FillHole.py
+++ FillHole.py
@@ -1,30 +1,30 @@
 #Module reparation
 import bpy
 import bmesh
+
+#selectionne l'objet
 context = bpy.context
 scene = context.scene
-# completely remove cube if in scene
-cube = scene.objects.get("Cube")
-if cube:
-    bpy.data.objects.remove(cube, do_unlink=True)
+
+objectToSelect = bpy.data.objects["Cube"]
+objectToSelect.select_set(True)    
+bpy.context.view_layer.objects.active = objectToSelect
+
+# cree le bmesh 
 bm = bmesh.new()
-# import from obj file here, only imported objects will be 
-# in context.selected_objects after import.    
 for o in context.selected_objects:
-    # test here to continue if not mesh, can obj import other???
-    # load bmesh
+    # charge bmesh
     bm.from_mesh(o.data)
-    # find boundaries
+    # trouve les frontieres
     bound_edges = set(e for e in bm.edges if e.is_boundary)
-    # used sets, may not be required.
     zero_edges = set(e for e in bound_edges 
         if all(abs(v.co.x) < 0.001 for v in e.verts))
     right_edges = bound_edges - zero_edges
-    # use bmesh "F" tool
+    # utilise bmesh tool
     result = bmesh.ops.contextual_create(bm, geom=list(bound_edges))
-    # poke the resulting faces to make triangular fan.
+    # affiche le resultat
     bmesh.ops.poke(bm, faces=result["faces"])
     bm.to_mesh(o.data)
-    o.data.update() # mainly only need for UI really
+    o.data.update()
     bm.clear()
 bm.free()
