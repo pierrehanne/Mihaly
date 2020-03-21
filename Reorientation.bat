49d2892e8d5547f5593e75e8ffefbdff943ec493
pierrehanne <43729077+pierrehanne@users.noreply.github.com>
2020-03-21 15:09:37 +0100

    Delete Reorientation.bat

--- Reorientation.bat
+++
@@ -1,7 +0,0 @@
-ECHO "Start";
-cd ../..
-cd "Blender Foundation"
-cd "Blender 2.81"
-blender.exe "C:\Users\jacky\Desktop\Mihally Project\Mihaly-master\Figures Tests\test.blend" -b -P "C:\Users\jacky\Desktop\Mihally Project\Mihaly-master\Reorientation.py" -o //result -f +1
-ECHO "Finish";
-PAUSE
